8596e0baab803a2b7c98d02b39dd789befd40eae
Niaz est <estefaie.niaz@gmail.com>
2022-11-01 14:43:07 +0330

    refactor: :fire: counter removed from the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import reactLogo from "./assets/react.svg";
-import { useAppDispatch, useAppSelector } from "./app/hooks";
-import { incremented, amountAdded } from "./features/counter/counter-slice";
 import { DatePicker } from "./components/DatePicker";
 import "./App.css";
 
 function App() {
-  const count = useAppSelector((state) => state.counter.value);
-  const dispatch = useAppDispatch();
-
-  const handleClick = () => {
-    dispatch(amountAdded(20));
-  };
-
   return (
     <div className="App">
       <DatePicker />
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counter-slice";
 import yearReducer from "../features/date/years-slice";
 import monthReducer from "../features/date/month-slice";
 import weekReducer from "../features/date/weeks-slice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     year: yearReducer,
     month: monthReducer,
     week: weekReducer,
