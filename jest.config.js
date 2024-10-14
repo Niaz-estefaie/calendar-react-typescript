af605dce6665b7d4f36d0996c87a1cab5d9f2074
Niaz <niaz.e@bugloos.com>
2024-10-14 16:32:49 +0330

    (chore): test added to the project

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  };
+  
